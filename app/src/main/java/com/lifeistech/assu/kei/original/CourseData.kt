deaeb78a70648ac9e99577bc8996ae62092654aa
KeitaroFukuda <keitaro.fukuda814@gmail.com>
2020-12-13 15:10:16 +0900

    Fifth Commit

--- app/src/main/java/com/lifeistech/assu/kei/original/CourseData.kt
+++ app/src/main/java/com/lifeistech/assu/kei/original/CourseData.kt
@@ -1,3 +1,6 @@
 package com.lifeistech.assu.kei.original
 data class CourseData(val productimage:Int,
-                      val productname:String)
+                      val productname:String,
+                      val producturl:String,
+                      val productbarcode:String)
+
---
+++ app/src/main/java/com/lifeistech/assu/kei/original/CourseData2.kt
@@ -0,0 +1,3 @@
+package com.lifeistech.assu.kei.original
+
+class CourseData2(val Wishlist
